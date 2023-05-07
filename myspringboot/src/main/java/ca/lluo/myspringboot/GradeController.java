beed1f35d86b84e63e7de7139e72658701c8d663
luolisave <luolisave@gmail.com>
2023-05-07 13:48:23 -0400

    session #88, grades list.

--- myspringboot/src/main/java/ca/lluo/myspringboot/GradeController.java
+++ myspringboot/src/main/java/ca/lluo/myspringboot/GradeController.java
@@ -1,5 +1,7 @@
 package ca.lluo.myspringboot;
 
+import java.util.*;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,17 +9,31 @@ import org.thymeleaf.engine.AttributeName;
 
 @Controller
 public class GradeController {
+
+    // List<Grade> studentGrades = new ArrayList<>();
+    List<Grade> studentGrades = Arrays.asList(
+        new Grade( "Harry" ,  "Potion", "C+"),
+        new Grade( "Jam" ,  "Arithmany", "B+"),
+        new Grade( "Lucy" ,  "IT", "A-")
+    );
+
+    @GetMapping("/grades")
+    public String getGrades(Model model) {
+        model.addAttribute("grades", studentGrades);
+        return "grades";
+    }
+
     @GetMapping("/hello")
     public String SayHello() {
         String SomeStr = "SayHello()";
         return "helloview";
     }
 
-    @GetMapping("/grades")
-    public String getGrades(Model model) {
+    @GetMapping("/test")
+    public String getTest(Model model) {
         Grade grade = new Grade( "Harry" ,  "Potion", "C+");
         model.addAttribute("grade", grade);
         model.addAttribute("sales", 150);
-        return "grades";
+        return "test";
     }
 }
